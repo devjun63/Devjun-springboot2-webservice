4fd2de83105ca68bbcd1b211066f5c3dc4b9ce5f
devjun <devjun63@gmail.com>
2020-02-15 22:39:17 +0900

    Google oauth setting and .gitignore success to application-oauth.properties

---
+++ src/main/java/com/devjun/intellij/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.devjun.intellij.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@EnableJpaAuditing //Jpa Auditing 활성화
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){ SpringApplication.run(Application.class, args); }
+}
---
+++ src/main/java/com/devjun/intellij/springboot/web/PostApiController.java
@@ -0,0 +1,37 @@
+package com.devjun.intellij.springboot.web;
+
+
+import com.devjun.intellij.springboot.service.PostsService;
+import com.devjun.intellij.springboot.web.dto.PostsResponseDto;
+import com.devjun.intellij.springboot.web.dto.PostsSaveRequestDto;
+import com.devjun.intellij.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update (@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+}
